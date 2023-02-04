89929a0771bad27d4af3764e3722ed22abcd43c0
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-02-04 20:04:00 +0600

    third

---
+++ Dom.js
@@ -0,0 +1,4 @@
+//getElementById
+
+document.getElementById("heading1").innerHTML = "Hello World!";
+document.querySelector("#para").innerHTML = "Bye";
---
+++ dist/Dom.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+//getElementById
+document.getElementById("heading1").innerHTML = "Hello World!";
+document.querySelector("#para").innerHTML = "Bye";
